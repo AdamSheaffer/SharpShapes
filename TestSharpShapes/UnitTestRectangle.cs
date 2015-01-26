c432c8b45cabb50d90f75beddbbdb29eb99065a7
adamsheaffer <adam.e.sheaffer@gmail.com>
2015-01-26 11:24:43 -0600

    created rectangle class and constructor tests

---
+++ TestSharpShapes/UnitTestRectangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpShapes;
+
+namespace TestSharpShapes
+{
+    [TestClass]
+    public class UnitTestRectangle
+    {
+        [TestMethod]
+        public void TestRectangleConstructor()
+        {
+            Rectangle rectangle = new Rectangle(40, 50);
+            Assert.AreEqual(40, rectangle.Width);
+            Assert.AreEqual(50, rectangle.Height);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleConstructorSanityCheckWidth()
+        {
+            Rectangle rectangle = new Rectangle(0, 50);
+            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleConstructorSanityCheckHeight()
+        {
+            Rectangle rectangle = new Rectangle(50, 0);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleConstructorSanityCheckNegativeHeight()
+        {
+            Rectangle rectangle = new Rectangle(40, -50);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleConstructorSanityCheckNegativeWidth()
+        {
+            Rectangle rectangle = new Rectangle(-40, 50);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleConstructorWithNoArgs()
+        {
+            Rectangle rectangle = new Rectangle();
+        }
+    }
+}
---
+++ TestSharpShapes/UnitTestShape.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpShapes;
+using System.Drawing;
+
+namespace TestSharpShapes
+{
+    public class MyShape : Shape
+    {
+        public override decimal Area()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Perimeter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Scale(int percent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int SidesCount
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    [TestClass]
+    public class UnitTestShape
+    {
+        [TestMethod]
+        public void TestSettingBorderColor()
+        {
+            Shape shape = new MyShape();
+            shape.BorderColor = Color.AliceBlue;
+            Assert.AreEqual(Color.AliceBlue, shape.BorderColor);
+        }
+
+        [TestMethod]
+        public void TestSettingFillColor()
+        {
+            Shape shape = new MyShape();
+            shape.FillColor = Color.AliceBlue;
+            Assert.AreEqual(Color.AliceBlue, shape.FillColor);
+        }
+
+        [TestMethod]
+        public void TestDefaultFillColor()
+        {
+            Shape shape = new MyShape();
+            Assert.AreEqual(Color.Tomato, shape.FillColor);
+        }
+
+        [TestMethod]
+        public void TestDefaultBorderColor()
+        {
+            Shape shape = new MyShape();
+            Assert.AreEqual(Color.Bisque, shape.BorderColor);
+        }
+    }
+}
